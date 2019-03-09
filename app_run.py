aef842e61571f95118ba389ada3b8e707228da8c
etrex.kuo <kamigo.service@gmail.com>
2019-03-09 21:24:45 +0800

    second ver

--- app_run.py
+++ app_run.py
@@ -1,45 +1,75 @@
+#from _future_ import print_function
+#from apiclient.discovery import build
+#from httplib2 import Http
+#from oauth2client import file, client, tools
+
+#import time
+#import re
+#import datetime
+#import random
+#import codecs
+#import sys
+#import json
+
 from flask import Flask, request, abort
+from urllib.request import urlopen
+#from oauth2client.service_account import ServiceAccountCredentials
 
 from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+    InvalidSignatureError,LineBotApiError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+
+################################
+
+from linebot.models import *
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('Ea2xoLFVBreUKwz1iurlzPmg/IVIPQaghPlbBqasGD7ZMyRe/MsULahoSIDruqffS9jCPN1RlLRTtHvkYR15pirANB51USl+rdYgWi6V0mxcEyvLnCJjfcYZljyi2V5VAz37eVWAxv9zoDVHujkZwQdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('a23f2e3d9172dd81ee5d480361d5a665')
+# Channel Access Token
+line_bot_api = "Ea2xoLFVBreUKwz1iurlzPmg/IVIPQaghPlbBqasGD7ZMyRe/MsULahoSIDruqffS9jCPN1RlLRTtHvkYR15pirANB51USl+rdYgWi6V0mxcEyvLnCJjfcYZljyi2V5VAz37eVWAxv9zoDVHujkZwQdB04t89/1O/w1cDnyilFU="')
+# Channel Secret
+handler = WebhookHandler('"a23f2e3d9172dd81ee5d480361d5a665"')
 
-
+# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
-
+# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    print(event)
+    text=event.message.text
 
+    if (text=="Hi"):
+        reply_text = "Hello"
+        #Your user ID
 
+    elif(text=="你好"):
+        reply_text = "哈囉"
+    elif(text=="機器人"):
+        reply_text = "叫我嗎"
+    else:
+        reply_text = text
+#如果非以上的選項，就會學你說話
+
+    message = TextSendMessage(reply_text)
+    line_bot_api.reply_message(event.reply_token, message)
+
+import os
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
