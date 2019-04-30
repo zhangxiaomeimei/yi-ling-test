26dfb75fdbbff2a06cff8861ef09095758b3bf2a
etrex.kuo <kamigo.service@gmail.com>
2019-04-30 16:00:32 +0800

    init

--- app.py
+++ app.py
@@ -341,7 +341,7 @@ def handle_message(event):
         return 0
 
     if event.message.text == "講義":
-    	Imagemap_Message = ImagemapSendMessage(
+        Imagemap_Message = ImagemapSendMessage(
             base_url='https://math-2019.000webhostapp.com/teacherlist.png?',
             alt_text='this is an imagemap',
             base_size=BaseSize(width=1061, height=540),
